9f34f52fcff8b6fa49691476c4634fd68398c87e
gustavoquinalha <gustavo_quinalha@hotmail.com>
2017-10-06 12:14:56 -0300

    buttons

--- components/btn/buttonComponent.vue
+++ components/btn/buttonComponent.vue
@@ -1,10 +1,16 @@
 <template>
 <div>
-    <button v-if="size === 'small'" type="button" name="button" class="btn btn-small" :class="[type]" :style="{ background: color, color: fontColor, borderRadius: borderRadius, width: width  }">{{value}}</button>
+  <div class="" v-if="size === 'small'">
+    <button type="button" name="button" class="btn btn-small" :class="[type]" :style="{ background: color, color: fontColor, borderRadius: borderRadius, width: width  }">{{value}}</button>
+  </div>
 
-    <button v-if="size === 'medium'" type="button" name="button" class="btn" :class="[type]" :style="{ background: color, color: fontColor, borderRadius: borderRadius, width: width  }">{{value}}</button>
+  <div class="" v-if="size === 'medium'">
+    <button type="button" name="button" class="btn" :class="[type]" :style="{ background: color, color: fontColor, borderRadius: borderRadius, width: width  }">{{value}}</button>
+  </div>
 
-    <button v-if="size === 'large'" type="button" name="button" class="btn btn-large" :class="[type]" :style="{ background: color, color: fontColor, borderRadius: borderRadius, width: width }">{{value}}</button>
+  <div class="" v-if="size === 'large'">
+    <button type="button" name="button" class="btn btn-large" :class="[type]" :style="{ background: color, color: fontColor, borderRadius: borderRadius, width: width }">{{value}}</button>
+  </div>
 
 </div>
 </template>
